dfe2b6d18ee25c4ac1036e796f2c80334cb60a93
Jacob Hoyos <hoyosj@etsu.edu>
2019-02-09 12:51:34 -0500

    Implemented AppReview basic structure

--- Shared/AbstractReview.cs
+++ Shared/AbstractReview.cs
@@ -11,6 +11,32 @@ namespace Shared
     /// </summary>
     public abstract class AbstractReview
     {
+        protected Student Subject;
+        protected DateTime ReviewDate;
+        
+        public AbstractReview(Student SubjectIn, DateTime DateIn)
+        {
+            Subject = SubjectIn; //Replace with a call to the copy constructor later
+            ReviewDate = DateIn;
+        }//end AbstractReview(Student, DateIn)
+
+        /// <summary>
+        /// Abstract method to add a Review to the tree
+        /// </summary>
+        /// <param name="Review"> The Review to add </param>
+        public abstract void Add(AbstractReview Review);
+
+        /// <summary>
+        /// Template method to remove a Review from the tree
+        /// </summary>
+        /// <param name="Review"> The Review to remove </param>
+        public abstract void Remove(AbstractReview Review);
+
+        /// <summary>
+        /// Template method to remove a Review at a specific position in the hosted list
+        /// </summary>
+        /// <param name="position"> The 0-based position from which to remove a Review </param>
+        public abstract void RemoveAt(int position);
 
     }//end AbstractReview
 }
--- Shared/AppReviews.cs
+++ Shared/AppReviews.cs
@@ -11,6 +11,33 @@ namespace Shared
     /// </summary>
     class AppReviews : AbstractReview
     {
+        List<AbstractReview> ReviewList = new List<AbstractReview>();
 
+        /// <summary>
+        /// The parameterized constructor that takes a student and datetime object, calls the parent class constructor
+        /// </summary>
+        /// <param name="subjectIn"> The student that is being reviewed </param>
+        /// <param name="DateIn"> The date of the review </param>
+        public AppReviews(Student subjectIn, DateTime DateIn) : base(subjectIn, DateIn)
+        {
+
+        }//end AppReview(Student, DateTime)
+
+        #region Necessary Overrides
+        public override void Add(AbstractReview Review)
+        {
+            ReviewList.Add(Review);
+        }//end Add(AbstractReview)
+
+        public override void Remove(AbstractReview Review)
+        {
+            ReviewList.Remove(Review);
+        }//end Remove(AbstractReview)
+
+        public override void RemoveAt(int position)
+        {
+            ReviewList.RemoveAt(position);
+        }//end RemoveAt(int)
+        #endregion
     }//end AppReviews
 }
